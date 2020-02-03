e78a11b517c05a6142d750c5acb941c3333e3c28
Ammar Shah <ammarshah123@gmail.com>
2020-02-04 00:17:44 +0500

    Mounting Phase with its Lifecycle Hooks (constructor, Render and componentDidMount)

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,6 +3,8 @@ import Counter from "./counter";
 
 class Counters extends Component {
   render() {
+    console.log("Counters - Rendered");
+
     const { onReset, counters, onIncrement, onDelete } = this.props;
     return (
       <div>
