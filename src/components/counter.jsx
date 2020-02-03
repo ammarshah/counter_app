c296e2fd08c5b02e8cd80d88b2baeb5a9b418bf8
Ammar Shah <ammarshah123@gmail.com>
2020-02-03 22:36:49 +0500

    Implements Single Source of Truth and removes local state of counter component which makes it a Controlled Component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,24 +1,12 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    value: this.props.counter.value
-  };
-
-  // To solve the problem of `this` referencing to a different object we created a constructor earlier.
-  // Another solution would be to convert the function into an arrow function and that will solve the problem.
-  // Because arrow functions don't re-bind the `this` keyword, they are inherited.
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   render() {
     return (
       <div>
-        <h4>Counter #{this.props.counter.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -35,14 +23,14 @@ class Counter extends Component {
 
   formatCount() {
     // Object Destructuring
-    // Using `value` property from `this.state` object and setting it to a const.
-    const { value } = this.state;
+    // Using `value` property from `this.props.counter` object and setting it to a const.
+    const { value } = this.props.counter;
     return value === 0 ? "Zero" : value;
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
